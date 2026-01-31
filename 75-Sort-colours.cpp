075d05368bc8a40b440638535b862843539cb013
Devansh Pandey <devanshpandey6869@gmail.com>
2026-01-31 15:42:40 +0530

    75-Sort-colours.cpp SOLUTION
    solution of 75-Sort-colours.cpp
     by three pointer approach kown as dutch national flag theorem

---
+++ 75-Sort-colours.cpp
@@ -0,0 +1,6 @@
+1> at global level this===?
+=>Window.
+
+2> JS known as loosely or weekly typed language.
+=>bcoz its variables adjust according to input there is no specific restriction that value should bhi int,string or etc.
+
